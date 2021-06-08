4142d90e4f15a957a1691102082c894e340dee9b
Cloud User <centos@ip-172-31-4-251.eu-west-2.compute.internal>
2021-06-08 12:35:41 +0000

    Complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Benjamin Sidebotham
+// Date: 08/06/21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -16,3 +16,31 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module led (
+	input clk,
+	input rst,
+	input button,
+	output reg [2:0] colour
+
+	);
+	
+	always @(posedge rst or button)
+		begin
+		if (rst)
+			colour = 3'b001;
+		else
+			case(colour)
+				3'b000: colour = 3'b001;
+				3'b001: colour = (button==1) ? 3'b010 : 3'b001;
+				3'b010: colour = (button==1) ? 3'b011 : 3'b010;
+				3'b011: colour = (button==1) ? 3'b100 : 3'b011;
+				3'b100: colour = (button==1) ? 3'b101 : 3'b100;
+				3'b101: colour = (button==1) ? 3'b110 : 3'b101;
+				3'b110: colour = (button==1) ? 3'b001 : 3'b110;
+				3'b111: colour = 3'b001;
+			endcase
+		end
+
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,111 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Benjamin Sidebotham
+// Date: 08/06/21
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb (
+	);
+
+	parameter CLK_PERIOD = 10;
+
+	reg clk;
+	reg rst;
+	reg button;
+	wire [2:0] colour;
+
+	reg err;
+	reg [2:0] colour_prev;
+	
+	//Clock generation
+	initial begin
+		clk = 1'b0;
+		forever 
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+	//Toggle button forever
+	initial begin
+		button = 0;
+		#(CLK_PERIOD*2);
+		button = 1;
+		forever
+			#(CLK_PERIOD) button=~button;					
+	end
+	
+	initial begin
+		
+		err=0;
+		rst=0;
+		#CLK_PERIOD;
+		rst=1;
+		#(CLK_PERIOD/2);
+		rst=0;
+		#(CLK_PERIOD/2);
+		colour_prev = colour;	
+		
+		forever begin
+			
+			// state should stay unchanged if button==0
+			colour_prev = colour;
+			#(CLK_PERIOD);
+			if (button==0 && colour!=colour_prev)
+				begin
+				$display("TEST FAILED - colour changed when button is 0");
+				err = 1;
+				end
+
+			// test all six colours
+			#CLK_PERIOD			
+			case(colour_prev)
+				3'b001: if (colour != 3'b010) begin
+					$display("TEST FAILED - 001 -> 010");
+					err = 1;
+					end
+				3'b010: if (colour != 3'b011) begin
+					$display("TEST FAILED - 010 -> 011");
+					err = 1;
+					end
+				3'b011: if (colour != 3'b100) begin
+					$display("TEST FAILED - 011 -> 100");
+					err = 1;
+					end
+				3'b100: if (colour != 3'b101) begin
+					$display("TEST FAILED - 100 -> 101");
+					err = 1;
+					end
+				3'b101: if (colour != 3'b110) begin
+					$display("TEST FAILED - 101 -> 110");
+					err = 1;
+					end
+				3'b110: if (colour != 3'b001) begin
+					$display("TEST FAILED - 110 -> 001");
+					err = 1;
+					end
+			endcase
+			colour_prev = colour;
+		end
+	end
+
+	//Finish test, check for success	
+     	initial begin
+        	#200;
+        	if (err==0)
+          		$display("***TEST PASSED***");
+       	 	$finish;
+      	end
+
+	//Instantiate counter module
+	led top (
+		.clk(clk),
+		.rst(rst),
+		.button(button),
+		.colour(colour)
+	);
+
+endmodule
