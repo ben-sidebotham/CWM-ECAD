462f0431a1cf5a59f82aea55c0b67a88216e5a60
Cloud User <centos@ip-172-31-4-251.eu-west-2.compute.internal>
2021-06-08 15:52:56 +0000

    Partial solution to ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Benjamin Sidebotham
+// Date: 08/06/21
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,61 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module aircon (
+	input clk,
+	input [4:0] temp,
+	output reg heating,
+	output reg cooling
+	);
+	// 00: HEATING ON ///// 01: IDLE ///// 10: COOLING ON
+	reg [1:0] state = 2'b01;
+	
+	always @(posedge clk) begin
+		case(state)
+			2'b00: begin
+				if (temp >= 5'b10100) begin //move to idle
+					state = 2'b01;
+					heating = 0;
+					cooling = 0;
+				end
+				else begin
+					state = 2'b00;  //remain at heating on
+					heating = 1;
+					cooling = 0;
+				end
+			end
+			2'b01: begin
+				if (temp <= 5'b10010) begin // move to heating on
+					state = 2'b00;
+					heating = 1;
+					cooling = 0;
+				end
+				else if (temp >= 5'b10110) begin //move to cooling
+					state = 2'b10;
+					heating = 0;
+					cooling = 1;
+				end
+				else begin // stay at idle
+					state = 2'b01;
+					heating = 0;
+					cooling = 0;
+				end
+			end
+			2'b10: begin
+				if (temp <= 5'b10010) begin //move to idle
+					state = 2'b01;
+					heating = 0;
+					cooling = 0;
+				end
+				else begin // stay at cooling
+					state = 2'b10;
+					heating = 0;
+					cooling = 1;
+				end
+			end
+		endcase
+	end
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,76 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Benjamin Sidebotham
+// Date: 08/06/21
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+	parameter CLK_PERIOD = 10;
+
+	reg clk;
+	reg [4:0] temp;
+	wire heating;
+	wire cooling;
+
+	reg err = 0;
+	reg up_down = 1;
+	reg [4:0] state_prev;
+	
+	initial begin   	
+	   clk = 1'b0;
+	   forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+	initial	begin
+	   forever begin
+		#CLK_PERIOD
+		if(cooling == 1 && heating == 1) begin
+		   $display("TEST FAILED - cooling and heating can not be both on");
+		   err=1;
+		end
+	   end
+	end
+
+	//test by cycling temp between 16 and 24
+	initial begin
+	   temp = 5'b10000;
+	   forever begin			
+		if (temp <= 5'b10000)
+			up_down = 1;
+		else if (temp >= 5'b11000)
+			up_down = 0;
+		else if (up_down == 1) begin
+			#CLK_PERIOD
+			temp = temp + 5'b00001;
+		end
+		else if (up_down == 0) begin
+			#CLK_PERIOD
+			temp = temp - 5'b00001;
+		end	
+	   end
+	end
+
+		
+	// finish test
+	initial begin
+		#200
+		if (err==0)
+			$display("TEST PASSED");
+		$finish;
+	end	
+	
+	aircon top (
+	.temp(temp),
+	.clk(clk),
+	.heating(heating),
+	.cooling(cooling)
+	);
+
+endmodule
