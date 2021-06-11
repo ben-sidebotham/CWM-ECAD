2267fc359cd18f4a5510d47b28e126e989b35777
Cloud User <centos@ip-172-31-4-251.eu-west-2.compute.internal>
2021-06-11 14:33:20 +0000

    Update 1

--- Ex9/project.tcl
+++ Ex9/project.tcl
@@ -23,6 +23,12 @@ set_property is_enabled true [get_files ${project_constraints}]
 create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name myblkmem
 set_property -dict [list CONFIG.Component_Name {myblkmem} CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/CWM-ECAD/Ex6/mem.coe}] [get_ips myblkmem]
 
+# Add files
+read_verilog "blinds.v"
+read_verilog "led.v"
+read_verilog "colcon.v"
+read_verilog "mux.v"
+read_verilog "selector.v"
 read_verilog "aircon.v"
 read_verilog "top.v"
 #read_verilog "top_tb.v"
--- Ex9/top.v
+++ Ex9/top.v
@@ -9,18 +9,21 @@
 //
 //	Plan:
 //	I want to add the light selector and electric roller blinds to the design from Ex8.
-//	Copy modules from prev exercises
-//	Copy mem.coe
-//	Add IP lines to project.tcl
-//	Add ports to top.v
-//	Add modules to project.tcl
+//
+//	Copy modules from prev exercises √
+//	Copy mem.coe √
+//	Add IP lines to project.tcl √
+//	Add ports to top.v √
+//	Add read_verilog for all modules to project.tcl √
+//	Edit constraints.xdc
+//	Write top_tb
 //
 //  inputs:
 //           rst_n, clk_n, clk_p, temperature [4:0], 
 //  	     button, func [3:0], a, b
 //
 //  outputs:
-//           heating, cooling, roller_out
+//           heating, cooling, roller_out, light [23:0]
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -39,6 +42,10 @@ module top(
 
 	// Todo: add ports for light selector
 	input button,
+	input sel,
+	output light_0, light_1, light_2, light_3, light_4, light_5, light_6, light_7, 
+	output light_8, light_9, light_10, light_11, light_12, light_13, light_14, light_15,
+	output light_16, light_17, light_18, light_19, light_20, light_21, light_22, light_23,
 
 	// Todo: add ports for roller blinds
 	input a,
@@ -49,8 +56,10 @@ module top(
 	input func_3,
 	output roller_out
 
-
 	);
+
+	// collect lights
+	wire [23:0] light_out 
 
    /* clock infrastructure, do not modify */
 	wire clk_ibufds;
@@ -67,13 +76,30 @@ module top(
 	.I  (clk_ibufds),
 	.O  (clk)
       );
-
-	//Add logic here
+	
+	// Modules
 	aircon aircon_0 (
 		.clk(clk),
 		.temp({temperature_4,temperature_3,temperature_2,temperature_1,temperature_0}),
 		.heating(heating),
 		.cooling(cooling)
 	);	
+	// Add other modules
+	blinds blinds_0 (
+		.a (a),
+		.b (b),
+		.func({func_3,func_2,func_1,func_0}),
+		.roller_out(roller_out)
+	);
+
+	selector selector_0 (
+		.clk(clk_p),
+		.sel(sel),
+		.rst(rst_n),
+		.button(button),
+		.light({light_23,light_22,light_21,light_20,light_19,light_18,light_17,light_16,
+			light_15,light_14,light_13,light_12,light_11,light_10,light_9,light_8,
+			light_7,light_6,light_5,light_4,light_3,light_2,light_1,light_0})
+	);
 
 endmodule
